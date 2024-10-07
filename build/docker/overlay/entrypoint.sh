bc89a15d3acacf32251c79b856198eaebbf4cd62
Igor <igor.sadza@gmail.com>
2024-10-07 22:41:39 +0200

    feat: added continus monitoring in start scripts

--- build/docker/overlay/entrypoint.sh
+++ build/docker/overlay/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Make sure that everything under ${HOME} belongs to user 
-sudo -E chmod -R a+rwX ${HOME}
-sudo -E chown -R ${USER_UID}:${USER_UID} ${HOME}
+#sudo -E chmod -R a+rwX ${HOME}
+#sudo -E chown -R ${USER_UID}:${USER_UID} ${HOME}
 
 # Change Permissions for config opt 
-sudo chmod -R a+rwX /opt
-sudo chown -R ${USER_UID}:${USER_UID} /opt
+#sudo chmod -R a+rwX /opt
+#sudo chown -R ${USER_UID}:${USER_UID} /opt
 
 # Execute all container core init scripts
 for init_script in /etc/cont-init.d/core/*.sh; do
