819c770e40ab6f6247a9ef73aef8c5e9b6baa81c
root <root@DELEQ0283001232.corp.capgemini.com>
2024-10-08 03:09:20 +0200

    feat: minor changes

--- setup/ansible/run_ansible.sh
+++ setup/ansible/run_ansible.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo -e "->Installing dependencies" \
+# Add google DNS server
+echo "nameserver 8.8.8.8" \
+	| sudo tee /etc/resolv.conf > /dev/null
+
+# Install Tools 
+echo -e "->Installing Tools" \
   && apt-get -qq update \
   && apt-get -qq install -y \
        python3 \
@@ -9,8 +14,7 @@ echo -e "->Installing dependencies" \
        sshpass \
        jq
 
-# -----------------------
-
+# Execute Ansible Playbook 
 echo -e "->Executing Ansible" \
   && TARGET_HOST=localhost \
      LC_ALL=C.UTF-8 \
