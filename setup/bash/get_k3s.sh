9f4548e2bd322cae4aaa144c08dd4002f07caf12
Igor <igor.sadza@gmail.com>
2024-10-08 09:37:18 +0200

    feat: add setup for bash-bash driven

--- setup/bash/get_k3s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -sfL https://get.k3s.io | sh -s -
-echo -e KUBECONFIG=/etc/rancher/k3s/k3s.yaml | tee -a /etc/environment
--- setup/bash/get_k9s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -sS https://webinstall.dev/k9s | bash
-ln -s /.local/bin/k9s /usr/bin/k9s
